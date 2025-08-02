8679e3fabeb31d878d39c5e409325ed6a8df135f
ashcircle03 <ashcircle03@gmail.com>
2025-08-02 19:55:00 +0900

    update

--- src/components/HandSilhouette.js
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from 'react';
-
-// Use forwardRef to pass the ref to the underlying canvas element
-// This component no longer has its own logic; it's just a styled canvas.
-const HandSilhouette = forwardRef((props, ref) => (
-  <canvas ref={ref} className="hand-silhouette" />
-));
-
-export default HandSilhouette;
